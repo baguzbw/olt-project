e35a9c1e6bf30f86247b81348adcc082251ccee5
baguzbw <bagusbwicaksono5@gmail.com>
2023-11-16 14:24:01 +0700

    BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, HashRouter as Router, Routes } from "react-router-dom";
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Device from "./Device";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
